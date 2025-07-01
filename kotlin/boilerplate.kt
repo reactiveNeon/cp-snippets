f5ea782e030fe49fcf1462997a369ea58f39f2da
Nishant Bhat <97377851+reactiveNeon@users.noreply.github.com>
2025-07-01 14:53:48 +0530

    Update boilerplate.kt

--- kotlin/boilerplate.kt
+++ kotlin/boilerplate.kt
@@ -1,20 +1,10 @@
-fun dbg(vararg args: Any?) {
-    val line = Throwable().stackTrace[1].lineNumber
-    var autoIndex = 1
-    val out = args.joinToString("  ") {
-        when (it) {
-            is Pair<*, *> -> "${it.first}: ${format(it.second)}"
-            else -> "_${autoIndex++}: ${format(it)}"
-        }
-    }
-    println("[$line] $out")
-}
+fun dbg(vararg args: Any?) { val line = Throwable().stackTrace[1].lineNumber; var i = 1; println("[$line] " + args.joinToString("  ") { if (it is Pair<*, *>) "${it.first}: ${format(it.second)}" else "_${i++}: ${format(it)}" }) }
 
 fun format(x: Any?): String = when (x) {
     is Pair<*, *> -> "(${format(x.first)}, ${format(x.second)})"
     is Triple<*, *, *> -> "(${format(x.first)}, ${format(x.second)}, ${format(x.third)})"
-    is Iterable<*> -> x.joinToString(separator = ", ", prefix = "{", postfix = "}") { format(it) }
-    is Array<*> -> x.joinToString(separator = ", ", prefix = "{", postfix = "}") { format(it) }
-    is Map<*, *> -> x.entries.joinToString(separator = ", ", prefix = "{", postfix = "}") { "${format(it.key)}: ${format(it.value)}" }
+    is Iterable<*> -> x.joinToString(", ", "{", "}") { format(it) }
+    is Array<*> -> x.joinToString(", ", "{", "}") { format(it) }
+    is Map<*, *> -> x.entries.joinToString(", ", "{", "}") { "${format(it.key)}: ${format(it.value)}" }
     else -> x.toString()
 }
