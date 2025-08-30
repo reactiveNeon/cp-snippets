967cbc630d8ca67979f5bd89945cc5aa66b91f3f
Nishant Bhat <97377851+reactiveNeon@users.noreply.github.com>
2025-08-30 21:18:15 +0530

    added 8 and 16 byte integer type aliases

--- cpp/leetcode/boilerplate.cpp
+++ cpp/leetcode/boilerplate.cpp
@@ -19,6 +19,6 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout
 #define MP make_pair
 #define MT make_tuple
 #define PII pair<int, int>
-using i32 = int32_t; using i64 = int64_t; using u32 = uint32_t; using u64 = uint64_t;
+using i8 = int8_t; using i16 = int16_t; using i32 = int32_t; using i64 = int64_t; using u8 = uint8_t; using u16 = uint16_t; using u32 = uint32_t; using u64 = uint64_t;
 template<typename T> T clamp(T x, T lo, T hi) { return min(hi, max(lo, x)); }
 auto fastio = [](){ ios::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
